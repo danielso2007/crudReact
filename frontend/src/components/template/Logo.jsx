d58df73f7abf6edcf5997e258d18a84f340866cd
danielso2007 <danielso2007@gmail.com>
2019-04-16 01:57:42 -0300

    feat: Componente Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,11 +1,15 @@
 import './Logo.css';
 import React, { Component } from 'react';
 
+import logo from '../../assets/images/logo.png';
+
 class Logo extends Component {
     render() {
         return (
             <aside className='logo'>
-                Logo
+                <a href='/' className='logo'>
+                    <img src={logo} alt='Logo' />
+                </a>
             </aside>
         );
     }
