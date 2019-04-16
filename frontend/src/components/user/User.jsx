4f4605c5aacf6eb1dfb7ffb092ab42baf58bf13c
danielso2007 <danielso2007@gmail.com>
2019-04-16 03:07:20 -0300

    feat(Parte 02): Componente Cadastro Usuário #02.

--- frontend/src/components/user/User.jsx
+++ frontend/src/components/user/User.jsx
@@ -1,14 +1,42 @@
 import './User.css';
 import React, { Component } from 'react';
 import Main from '../template/Main';
+import axios from 'axios';
 
 const headerProps = {
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir.'
-}
+};
+
+const baseUrl = 'http://localhost:3001/users';
+
+const initialState = { user: {name: '', email: ''}, list: [] }
 
 class User extends Component {
+
+    state = { ...initialState };
+
+    clear() {
+        this.setState({user: initialState.user});
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id);
+        list.unshift(user);
+        return list;
+    }
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+        axios[method](url, user).then(resp => {
+            const list = this.getUpdatedList(resp.data);
+            this.setState({user: initialState.user, list});
+        });
+    }
+
     render() {
         return (
             <Main {...headerProps}>
